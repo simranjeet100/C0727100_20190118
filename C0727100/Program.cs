9c277799ba6e130471dcc375fe6d5e1913d87070
visualstudio <visualstudio@122-PC18>
2019-01-18 19:15:43 -0500

    Second Commit

--- C0727100/Program.cs
+++ C0727100/Program.cs
@@ -10,6 +10,10 @@ namespace C0727100
     {
         static void Main(string[] args)
         {
+            int i = 5;
+            Console.WriteLine(i);
+            Console.WriteLine(i+1);
+            Console.ReadLine();
         }
     }
 }
